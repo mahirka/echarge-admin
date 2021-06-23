48e906117854642257a281a106ba0e2fafbba834
mahir <mahirka4@gmail.com>
2021-06-23 17:40:17 +0530

    new changes

--- src/app/dashboard/dashboard-home/dashboard-home.component.ts
+++ src/app/dashboard/dashboard-home/dashboard-home.component.ts
@@ -1,3 +1,4 @@
+import { NONE_TYPE } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Color } from 'ng2-charts';
@@ -154,19 +155,325 @@ export class DashboardHomeComponent implements OnInit {
     responsive: true,
     maintainAspectRatio: false
   };
-  isLoading = false;
 
+  public usersDatasets = [
+    {
+      label: 'My First dataset',
+      data: [65, 59, 80, 81, 46, 55, 38, 59, 80],
+      datalabels: {
+        display: false,
+      },
+
+    }
+  ];
+
+  public usersLineChartColors: Color[] = [
+    { // dark grey
+      backgroundColor: 'darkseagreen',
+      borderColor: 'deepskyblue',
+      borderCapStyle: 'round',
+      borderDash: [],
+      borderWidth: 4,
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'round',
+      pointBorderColor: 'deepskyblue',
+      pointBackgroundColor: 'white',
+      pointHoverBorderWidth: 4,
+      pointRadius: 6,
+      pointBorderWidth: 5,
+      pointHoverRadius: 8,
+      pointHitRadius: 10,
+      pointHoverBackgroundColor: 'white',
+      pointHoverBorderColor: 'deepskyblue',
+    },
+  ];
+
+  public usersLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August'];
+
+  public usersOptions = {
+    layout: {
+      padding: {
+        left: 0,
+        right: 8,
+        top: 0,
+        bottom: 0
+      }
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          display: false,
+          beginAtZero: true
+        },
+        gridLines: {
+          display: false
+        }
+      }],
+      xAxes: [{
+        ticks: {
+          display: false
+        },
+        gridLines: {
+          display: false
+        }
+      }]
+    },
+    legend: {
+      display: false
+    },
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+  public vendorsDatasets = [
+    {
+      label: 'My First dataset',
+      data: [65, 59, 80, 81, 46, 55, 38, 59, 80],
+      datalabels: {
+        display: false,
+      },
+
+    }
+  ];
+
+  public vendorsLineChartColors: Color[] = [
+    { // dark grey
+      backgroundColor: 'lightblue',
+      borderColor: 'yellow',
+      borderCapStyle: 'round',
+      borderDash: [],
+      borderWidth: 4,
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'round',
+      pointBorderColor: 'yellow',
+      pointBackgroundColor: 'white',
+      pointHoverBorderWidth: 4,
+      pointRadius: 6,
+      pointBorderWidth: 5,
+      pointHoverRadius: 8,
+      pointHitRadius: 10,
+      pointHoverBackgroundColor: 'white',
+      pointHoverBorderColor: 'yellow',
+    },
+  ];
+
+  public vendorsLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August'];
+
+  public vendorsOptions = {
+    layout: {
+      padding: {
+        left: 0,
+        right: 8,
+        top: 0,
+        bottom: 0
+      }
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          display: false,
+          beginAtZero: true
+        },
+        gridLines: {
+          display: false
+        }
+      }],
+      xAxes: [{
+        ticks: {
+          display: false
+        },
+        gridLines: {
+          display: false
+        }
+      }]
+    },
+    legend: {
+      display: false
+    },
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+  public stationDatasets = [
+    {
+      label: 'My First dataset',
+      data: [65, 59, 80, 81, 46, 55, 38, 59, 80],
+      datalabels: {
+        display: false,
+      },
+
+    }
+  ];
+
+  public stationLineChartColors: Color[] = [
+    { // dark grey
+      backgroundColor: 'lightcoral',
+      borderColor: 'darkseagreen',
+      borderCapStyle: 'round',
+      borderDash: [],
+      borderWidth: 4,
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'round',
+      pointBorderColor: 'darkseagreen',
+      pointBackgroundColor: 'white',
+      pointHoverBorderWidth: 4,
+      pointRadius: 6,
+      pointBorderWidth: 5,
+      pointHoverRadius: 8,
+      pointHitRadius: 10,
+      pointHoverBackgroundColor: 'white',
+      pointHoverBorderColor: 'darkseagreen',
+    },
+  ];
+
+  public stationLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August'];
+
+  public stationOptions = {
+    layout: {
+      padding: {
+        left: 0,
+        right: 8,
+        top: 0,
+        bottom: 0
+      }
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          display: false,
+          beginAtZero: true
+        },
+        gridLines: {
+          display: false
+        }
+      }],
+      xAxes: [{
+        ticks: {
+          display: false
+        },
+        gridLines: {
+          display: false
+        }
+      }]
+    },
+    legend: {
+      display: false
+    },
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+  public machineDatasets = [
+    {
+      label: 'My First dataset',
+      data: [65, 59, 80, 81, 46, 55, 38, 59, 80],
+      datalabels: {
+        display: false,
+      },
+
+    }
+  ];
+
+  public machineLineChartColors: Color[] = [
+    { // dark grey
+      backgroundColor: 'burlywood',
+      borderColor: 'darkcyan',
+      borderCapStyle: 'round',
+      borderDash: [],
+      borderWidth: 4,
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'round',
+      pointBorderColor: 'darkcyan',
+      pointBackgroundColor: 'white',
+      pointHoverBorderWidth: 4,
+      pointRadius: 6,
+      pointBorderWidth: 5,
+      pointHoverRadius: 8,
+      pointHitRadius: 10,
+      pointHoverBackgroundColor: 'white',
+      pointHoverBorderColor: 'darkcyan',
+    },
+  ];
+
+  public machineLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August'];
+
+  public machineOptions = {
+    layout: {
+      padding: {
+        left: 0,
+        right: 8,
+        top: 0,
+        bottom: 0
+      }
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          display: false,
+          beginAtZero: true
+        },
+        gridLines: {
+          display: false
+        }
+      }],
+      xAxes: [{
+        ticks: {
+          display: false
+        },
+        gridLines: {
+          display: false
+        }
+      }]
+    },
+    legend: {
+      display: false
+    },
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+
+
+  visibleGraph='none';
+  isLoading = false;
+  dashboardDetails = []
+  bookingCountsToShow:any[]=[]
+  booking_total: string = ""
+  users_total: string = ""
+  vendors_total: string = ""
+  stations_total: string = ""
+  machines_total: string = ""
+  co2_total: string = ""
+  tree_total: string = ""
+  time_total: string = ""
+  data_total: string = ""
+  
 
   constructor(private _router: Router,private _httpService: HttpService) { }
   ngOnInit(): void {
+    this.getdashboardDetails()
   }
   getdashboardDetails(){
     this._httpService.getdashboardDetails()
     .subscribe(
       data => {
-        console.log("usersProfile")
+        console.log("data")
         this.isLoading = false;
           console.log(data)
+         this.dashboardDetails=data['data']
+          this.booking_total = data['data']['orders']['total']
+          this.users_total = data['data']['users']['total']
+          this.vendors_total = data['data']['vendors']['total']
+          this.stations_total = data['data']['charging_stations']['total']
+          this.machines_total = data['data']['charging_machines']['total']
+          this.co2_total = parseFloat(data['data']['co2_saved']['total']['auto'] + data['data']['co2_saved']['total']['bike'] + data['data']['co2_saved']['total']['car']).toFixed(2)
+          this.tree_total = data['data']['number_of_tree_planed']
+          this.time_total = data['data']['total_charging_time']['total']
+          this.data_total = data['data']['total_charging_data']['total']
+          
+
+          console.log("total")
+          console.log(this.booking_total)
       
       },
       error => {
